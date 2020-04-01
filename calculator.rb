eedce1739ecb8e1d620ed42d14ffb78fbac79199
Omowonuola Jimoh <ojimoh21@live.co.uk>
2020-04-01 17:08:26 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,4 +2,5 @@ first_number = 5
 second_number = 8
 
 sum = first_number + second_number
-difference= first_number - second_number
+difference= first_number - second_number 
+product = first_number * second_number
